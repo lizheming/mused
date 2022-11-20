97720bcd1263d42f17c8dca8ff6fa94363c98368
lizheming <i@imnerd.org>
2022-11-20 23:02:03 +0800

    chore: update mused

--- packages/server/src/controller/index.js
+++ packages/server/src/controller/index.js
@@ -2,6 +2,6 @@ const BaseRest = require('./rest');
 
 module.exports = class extends BaseRest {
   getAction() {
-    return this.success('Welcome to use muse!');
+    return this.success('Welcome to use mused!');
   }
 }
